3a49fafb4d7ec3375ce389fb15593222f43bb832
Juho Ahonen <juho.ahonen2@gmail.com>
2022-02-04 16:09:58 +0200

    alcometer v1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
-function App() {
+function Alcometer() {
+  const [weight, setWeight] = useState(0);
+  const [bottles, setBottles] = useState(0);
+  const [time, setTime] = useState(0);
+  const [gender, setGender] = useState(0);
+  const [bloodLevel, setBloodLevel] = useState(0);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const litres = bottles * 0.33;
+    const grams = litres * 8 * 4.5;
+    const burning = weight / 10;
+    const gramsLeft = grams - (burning * time);
+    const finalResult = gramsLeft / (weight * gender)
+
+    if (gender == 0) {
+      setBloodLevel("Choose gender")
+      return
+    }
+    
+    if (finalResult > 0) {
+      setBloodLevel(finalResult.toFixed(2))
+    }
+    else {
+      setBloodLevel(0)
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <form onSubmit={handleSubmit}>
+      <div className="Alcometer">
+        <h1>Calculating alcohol blood level</h1>
+        <label for="weight">Weight</label>
+        <input
+          id="weight"
+          type="number"
+          value={weight}
+          onChange={(e) => setWeight(e.target.value)}
+        />
+        <br />
+        <label for="bottles">Bottles</label>
+        <select
+          name="bottles"
+          id="bottles"
+          onChange={(e) => setBottles(e.target.value)}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <option value="0">0</option>
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">6</option>
+          <option value="8">8</option>
+          <option value="9">9</option>
+          <option value="10">10</option>
+          <option value="11">11</option>
+          <option value="12">12</option>
+          <option value="13">13</option>
+        </select>
+        <br />
+        <label for="time">Time</label>
+        <select name="time" id="time" onChange={(e) => setTime(e.target.value)}>
+          <option value="0">0</option>
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">6</option>
+          <option value="7">7</option>
+          <option value="8">8</option>
+          <option value="9">9</option>
+          <option value="10">10</option>
+        </select>
+
+        <br />
+        <label>Gender</label>
+        <label for="male">Male</label>
+        <input type="radio"id="male"name="gender"value="0.7"onChange={(e) => setGender(e.target.value)} />
+        <label for="female">Female</label>
+        <input type="radio" id="female" name="gender" value="0.6" onChange={(e) => setGender(e.target.value)} />
+        <br />
+        <button>Calculate</button> <br />
+        <span>{bloodLevel}</span>
+      </div>
+    </form>
   );
 }
 
-export default App;
+export default Alcometer;
